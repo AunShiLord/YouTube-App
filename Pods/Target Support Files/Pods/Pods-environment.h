5dcb885eb9ba32fcf50398a80f150b26529efac6
AShi <aunshilord@yahoo.com>
2015-05-23 12:27:51 +0300

    Updated pods, fixed cocoa issues

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -76,5 +76,5 @@
 #define COCOAPODS_POD_AVAILABLE_youtube_ios_player_helper
 #define COCOAPODS_VERSION_MAJOR_youtube_ios_player_helper 0
 #define COCOAPODS_VERSION_MINOR_youtube_ios_player_helper 1
-#define COCOAPODS_VERSION_PATCH_youtube_ios_player_helper 2
+#define COCOAPODS_VERSION_PATCH_youtube_ios_player_helper 3
 
